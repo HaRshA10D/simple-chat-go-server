53a0b55ec967fd709c766e07f655fce90a17305c
Abhinav Rai <abhinav.rai.1996@gmail.com>
2018-11-27 01:21:57 +0530

    [Abhinav | Ashu] add migrate flag

--- chat_server.go
+++ chat_server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"source.golabs.io/ops-tech-peeps/simple-chat-go-server/model"
@@ -10,14 +11,22 @@ import (
 )
 
 func main() {
+	migrate := flag.Bool("migrate", false, "To run migration")
+	flag.Parse()
 	config := model.NewConfig()
 	store, err := store.NewSimpleChatStore(config)
 	if err != nil {
 		log.Fatalf("Can't connect to store : \n%s", err.Error())
 	}
-	err = store.InitDatabase()
-	if err != nil {
-		log.Fatalf("Can't connect to store : \n%s", err.Error())
+	if *migrate {
+		err = store.InitDatabase()
+		if err != nil {
+			log.Fatalf("Could not perform migration due to: %s", err.Error())
+		}
+		if store.DB() != nil {
+			store.DB().Close()
+		}
+		return
 	}
 	api := api.Init(store)
 	api.StartServer(config.ServerPort, config.WriteTimeout, config.ReadTimeout, config.IdleTimeout)
